1f7372c2c16694201937d82663b98019446eaea5
kaznacheeva <katya.kazn@gmail.com>
2018-01-14 19:20:19 +0300

    change port

--- server.js
+++ server.js
@@ -36,5 +36,5 @@ db.connect(uri, (err) => {
   if (err) {
     return console.log(err);
   }
-  app.listen(3000, () => console.log('Example app listening on port 3000!'))
+  app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
 })
